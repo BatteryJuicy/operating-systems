d91cbc2aa9534071be7df91baaf03ae870c03a90
batteryjuice@BatteryJuicer <csd5401@csd.uoc.gr>
2025-11-08 20:50:18 +0200

    read arguments with error check done

--- hy345/ask2/src/main.c
+++ hy345/ask2/src/main.c
@@ -4,12 +4,31 @@
 ============================================================================*/
 
 #include <stdio.h>
+#include <stdlib.h>
 
-#define N argv[0]
-#define G argv[]
-
+static int _N, _G, _X;
+const int *const N = &_N; //Nubmer of Tables
+const int *const G = &_G; //Number of Groups
+const int *const X = &_X; //Capacity of each Table and cap of Group Size
 
 int main(int argc, char* argv[])
 {
-    pritnf(":%s:", argv[0]);
+    if (argc != 4)
+    {
+        fprintf(stderr, "Invalid number of parameters.\n");
+        return 1;
+    }
+
+    _N = atoi(argv[1]);
+    _G = atoi(argv[2]);
+    _X = atoi(argv[3]);
+
+    if (*N < 1 || *G < 1 || *X < 1)
+    {
+        fprintf(stderr, "Invalid Parameter types or values.\n");
+        return 1;
+    }
+    printf(":%d, %d, %d:\n", *N, *G, *X);
+
+    return 0;
 }
